fbdcedf5227c698b2cf67459864c1739693bd20c
benicio <benicio.brito@fortaleza.ce.gov.br>
2016-12-08 14:27:15 -0300

    Adicionando exemplos jms conectando em um servidor Activemq

---
+++ src/br/edu/fa7/TesteConsumidorFila.java
@@ -0,0 +1,54 @@
+package br.edu.fa7;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class TesteConsumidorFila {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws Exception {
+
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection connection = factory.createConnection();
+		connection.start();
+
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+		Destination fila = (Destination) context.lookup("financeiro");
+		MessageConsumer consumer = session.createConsumer(fila);
+		// Message message = consumer.receive();
+
+		consumer.setMessageListener(new MessageListener() {
+
+			@Override
+			public void onMessage(Message message) {
+			    TextMessage textMessage  = (TextMessage)message;
+		        try{
+		            System.out.println(textMessage.getText());
+		        } catch(JMSException e){
+		            e.printStackTrace();
+		        } 
+
+			}
+		});
+
+		new Scanner(System.in).nextLine();
+
+		session.close();
+		connection.close();
+		context.close();
+
+	}
+
+}
---
+++ src/br/edu/fa7/TesteProdutorTopico.java
@@ -0,0 +1,38 @@
+package br.edu.fa7;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+
+public class TesteProdutorTopico {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws Exception {
+
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection connection = factory.createConnection();
+		connection.start();
+
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+		Destination topico = (Destination) context.lookup("loja");
+
+		MessageProducer producer = session.createProducer(topico);
+
+		Message message = session.createTextMessage("<pedido><id>" + 234 + "</id></pedido>");
+		
+		producer.send(message);
+
+		// new Scanner(System.in).nextLine();
+
+		session.close();
+		connection.close();
+		context.close();
+
+	}
+
+}
